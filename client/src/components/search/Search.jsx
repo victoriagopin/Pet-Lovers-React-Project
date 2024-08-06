999255465fa3db99bb1a5c3f846a7d543d09c95a
Victoria Gopin <145561051+victoriagopin@users.noreply.github.com>
2024-08-06 16:45:37 +0300

    Used custom hook for form; fixed logic

--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -1,41 +1,50 @@
 import { useState } from 'react';
 import styles from './Search.module.css';
 import SearchList from './search-list/Search-list';
+import { useForm } from '../../hooks/useForm';
 
 const baseUrl = 'http://localhost:3030/data/foods'
 
-export default function Search() {
-    const [values, setValues] = useState({
+const initialValues ={
         weight: '',
         animal: 'dog',
         lifeStage: 'adult',
         breed: ''
-    });
-    
-    const [animalFood, setAnimalFood] = useState(null);
+}
 
-    const changeValues = (e) => {
-        setValues(oldValues => ({
-            ...oldValues,
-            [e.target.name] : e.target.value
-        }))
-    }
+export default function Search() {
+    const {values, changeHandler} = useForm(initialValues);
+    const [animalFood, setAnimalFood] = useState(null);
+    const [hasInvalidField, setHasInvalidField] = useState(false);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        setHasInvalidField(false);
+
+        if(!values.weight || !values.breed){
+            setHasInvalidField(true);
+            return;
+        }
 
         try{
             if(values.lifeStage == 'adult'){
                 const params = new URLSearchParams({
                     where: `animalType="${values.animal}"`
                 })
+                
                 const req = await fetch(`${baseUrl}?${params.toString()}`);
-    
+                
                 const res = await req.json();
-        
-                const food = res.find(petWeight => values.weight <= petWeight.weight);
+    
+                const adults = res.filter(pet => pet.lifeStage == values.lifeStage);
+                const food = adults.find(petWeight => values.weight <= petWeight.weight);
+              
                 if(food.breeds.includes(values.breed)){
-                setAnimalFood(food);
+                    setAnimalFood(food);
+                } else {
+                    setHasInvalidField(true);
+                    return;
                 }
             } else {
             const params = new URLSearchParams({
@@ -68,17 +77,17 @@ export default function Search() {
 
                         <div className={styles['form-outline']}>
                             <label className={styles['form-label']} htmlFor="kg">Weight (kg)</label>
-                            <input type="number" id="kg" className={styles['form-control']} name='weight' value={values.weight} onChange={changeValues}/>
+                            <input type="number" id="kg" className={styles['form-control']} name='weight' value={values.weight} onChange={changeHandler}/>
                         </div>
 
                         <div className={styles['form-outline']}>
                             <label className={styles['form-label']} htmlFor="breed">Breed</label>
-                            <input type="text" id="breed" className={styles['form-control']} name='breed' value={values.breed} onChange={changeValues}/>
+                            <input type="text" id="breed" className={styles['form-control']} name='breed' value={values.breed} onChange={changeHandler}/>
                         </div>
 
                         <div className={styles['form-outline']}>
                             <label className={styles['form-label']} htmlFor="age-unit">Pet</label>
-                            <select id="animal" className={styles['form-control']} name='animal' value={values.animal} onChange={changeValues}>
+                            <select id="animal" className={styles['form-control']} name='animal' value={values.animal} onChange={changeHandler}>
                                 <option value="dog">Dog</option>
                                 <option value="cat">Cat</option>
                             </select>
@@ -86,7 +95,7 @@ export default function Search() {
 
                         <div className={styles['form-outline']}>
                             <label className={styles['form-label']} htmlFor="age-unit">Pet Life Stage</label>
-                            <select id="life-stage" className={styles['form-control']} name='lifeStage' value={values.lifeStage} onChange={changeValues}>
+                            <select id="life-stage" className={styles['form-control']} name='lifeStage' value={values.lifeStage} onChange={changeHandler}>
                                 <option value="baby">Baby (up to 12 months)</option>
                                 <option value="adult">Adult</option>
                             </select>
@@ -98,14 +107,16 @@ export default function Search() {
                     </div>
                 </div>
             </div> 
-            {animalFood 
-                ?
-                    <SearchList 
-                    key={animalFood._id}
-                    foods={animalFood.food}
-                    />
-                :   null
-            }
+            {hasInvalidField ? 
+                    (<p className='error-search'>Please make sure there are no empty fileds and the data you have entered is correct!</p> )
+                    : (animalFood ?
+                        (<SearchList 
+                        key={animalFood._id}
+                        foods={animalFood.food}
+                        />
+                         ) :   null
+            )}
+           
         </section>
     );
 }
